5eb78e8618801cbe0e2a2a3258d3de01040a82dd
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-12-20 17:30:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,26 @@ client.on("ready", () => {
 });
 
 // listen for message
-client.on("message", msg => {
+client.on("message", async msg => {
     if (!msg.content.startsWith(PREFIX)) return;
     const args = msg.content.slice(PREFIX.length).split("/ +/");
     const command = args.shift().toLowerCase();
   
     // create commands
     if (command === "ping") return msg.reply("pong");
+    if (command === "people") return msg.reply("there are ${msg.guild.messageCount} membes")
+    if (command === "poll") {
+        let message = await msg.reply(args.join(" "));
+        await message.react("yes");
+        await message.react("no");
+    }
+    if (command === "avatar") {
+        const embed = new Discord.MessageEmbed()
+            .setTitle("User Avatar")
+            .setColor("#0099ff")
+            .setImage(msg.author.avatarURL())
+            .setThumbnail(msg.author.avatarURL())
+            .setTimestamp()
+        msg.channel.send({embeds: [embed]});
+    }
 });
